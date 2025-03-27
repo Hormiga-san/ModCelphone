ea37281e3438f87b6cfc7d99b9db38f1c225bdfa
Hormiga <azocarfrancisca03@gmail.com>
2025-03-26 22:03:22 -0300

    Delete //

--- src/main/java/net/hormiga/celphone/CelPhoneMain.java
+++ src/main/java/net/hormiga/celphone/CelPhoneMain.java
@@ -17,9 +17,9 @@ import org.slf4j.Logger;
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(CelPhoneMain.MOD_ID)
 public class CelPhoneMain {
-    // Define mod id in a common place for everything to reference
+
     public static final String MOD_ID = "celphone";
-    // Directly reference a slf4j logger
+
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public CelPhoneMain(FMLJavaModLoadingContext context) {
